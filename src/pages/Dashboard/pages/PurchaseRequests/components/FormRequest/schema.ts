6a2ba913c78e82e56b731c427710d988886a851e
Francis Soares <francis.soares@GRUPOSANTIN.LOCAL>
2024-01-05 10:28:02 -0300

    remove: modal

--- src/pages/Dashboard/pages/PurchaseRequests/components/FormRequest/schema.ts
+++ src/pages/Dashboard/pages/PurchaseRequests/components/FormRequest/schema.ts
@@ -2,8 +2,7 @@ import { z } from "zod";
 
 export const purchaseFormSchema = z.object({
   solicitante: z.string(),
-  fornecedor: z.string().min(5, "Obrigatório preencher o fornecedor"),
-  comprador: z.string(),
+  datanecessaria: z.string().min(1,"Obrigatório preencher a data necessária")
 });
 
 export type TPurchase = z.infer<typeof purchaseFormSchema>;
