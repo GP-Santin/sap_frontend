86b6d9755cb2926db96635b31726ba859f3bd11e
Ricardo José Rufino Junior <cadinhorufjr@icloud.com>
2024-01-17 17:44:20 -0300

    refactor: auth.ts

--- src/auth-config.ts
+++ src/auth-config.ts
@@ -5,7 +5,7 @@ const msalConfig = {
     clientId: "1e531b8f-47e5-4c51-b5ae-951b3787403e",
     authority:
       "https://login.microsoftonline.com/81a5961d-1cf6-41f7-8dc2-027022287f7d/v2.0",
-    redirectUri: "https://localhost:5173/",
+    redirectUri: "https://sap-bha5xrq5v-grupo-santin.vercel.app/",
   },
   cache: {
     cacheLocation: "sessionStorage",
