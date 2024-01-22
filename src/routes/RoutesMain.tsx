82449a6c8af5099de65fb09299c266f213a29fe2
Ricardo José Rufino Junior <cadinhorufjr@icloud.com>
2024-01-22 14:56:56 -0300

    remove unused packages

--- src/routes/RoutesMain.tsx
+++ src/routes/RoutesMain.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PrivatesRoutes from "./PrivatesRoutes";
 import PublicRoutes from "./PublicRoutes";
 import LoginPage from "../pages/Login/LoginPage";
@@ -10,38 +10,36 @@ import SAPRoutes from "./SapRoutes";
 function RoutesMain({ toggleTheme, theme }: INavProps) {
   const sessionSAP = localStorage.getItem("@session");
   return (
-    <HashRouter>
-      <Routes>
-        <Route element={<PublicRoutes />}>
-          <Route path="/" element={<LoginPage />} />
-        </Route>
-        <Route element={<PrivatesRoutes />}>
+    <Routes>
+      <Route element={<PublicRoutes />}>
+        <Route path="/" element={<LoginPage />} />
+      </Route>
+      <Route element={<PrivatesRoutes />}>
+        <Route path="/login" element={<HomePage />} />
+        {sessionSAP ? (
+          <Route element={<SAPRoutes />}>
+            <Route
+              path="/dashboard/purchase-requests"
+              element={
+                <PurchaseRequests toggleTheme={toggleTheme} theme={theme} />
+              }
+            />
+            <Route
+              path="/dashboard/regularization"
+              element={
+                <Regularization toggleTheme={toggleTheme} theme={theme} />
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={<Dashboard toggleTheme={toggleTheme} theme={theme} />}
+            />
+          </Route>
+        ) : (
           <Route path="/login" element={<HomePage />} />
-          {sessionSAP ? (
-            <Route element={<SAPRoutes />}>
-              <Route
-                path="/dashboard/purchase-requests"
-                element={
-                  <PurchaseRequests toggleTheme={toggleTheme} theme={theme} />
-                }
-              />
-              <Route
-                path="/dashboard/regularization"
-                element={
-                  <Regularization toggleTheme={toggleTheme} theme={theme} />
-                }
-              />
-              <Route
-                path="/dashboard"
-                element={<Dashboard toggleTheme={toggleTheme} theme={theme} />}
-              />
-            </Route>
-          ) : (
-            <Route path="/login" element={<HomePage />} />
-          )}
-        </Route>
-      </Routes>
-    </HashRouter>
+        )}
+      </Route>
+    </Routes>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-      onwarn: (warning, warn) => {
-        if (warning.code === "UNUSED_EXTERNAL_IMPORT") return;
-        warn(warning);
-      },
-    },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "./private.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "./certificate.crt")),
-    },
   },
 });
